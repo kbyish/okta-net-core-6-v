64d6caadf569b4a78007a31fcfb7844cd1e2d863
kbyish <kifah.byish@gmail.com>
2021-12-05 14:35:13 -0800

    add MessgaeNoAuth

---
+++ Member/Controllers/MessagesNoAuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Member.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesNoAuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var messages = new dynamic[]
+            {
+                new {Date= DateTime.Now, Text="Kifah Message No Auth"},
+                 new {Date= DateTime.Now, Text="Sanjay Message No Auth"},
+                  new {Date= DateTime.Now, Text="Samin Message No Auth"}
+
+            };
+
+            return Ok(messages);
+        }
+    }
+}
