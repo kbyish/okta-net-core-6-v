3c2dc746aadf4a527ec435c1db4c71710d9044ef
kbyish <kifah.byish@gmail.com>
2021-12-04 18:49:49 -0800

    start

---
+++ Member/Controllers/MessagesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Member.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MessagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var messages = new dynamic[]
+            {
+                new {Date= DateTime.Now, Text="Kifah Message"},
+                 new {Date= DateTime.Now, Text="Sanjay Message"},
+                  new {Date= DateTime.Now, Text="Samin Message"}
+
+            };
+
+            return Ok(messages);
+        }
+    }
+}
---
+++ Member/Program.cs
@@ -0,0 +1,52 @@
+using Okta.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
+    options.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
+    options.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
+})
+    .AddOktaWebApi(new OktaWebApiOptions()
+    {
+        OktaDomain = "https://dev-00314289.okta.com",
+    });
+
+builder.Services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+
+builder.Services.AddAuthorization();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
